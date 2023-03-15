e6f96e00e3115960a589a61a078c36b4a4fa062c
6Cript <102388136+6CRIPT@users.noreply.github.com>
2023-03-15 14:01:46 +0100

    Add files via upload

---
+++ TSR/practica1/PracticaTSR-1.1-ejemplos-1/PracticaTSR-1.1-ejemplos/probando.js
@@ -0,0 +1,26 @@
+// console.log(x)
+// var x = 1
+// console.log(x)
+// let i = 0
+// let i = 1
+// console.log(i)
+// var x;
+// for (x=11; x<14; x++) {
+// 	console.log (x)
+// }
+// function f(y){
+// 	var x=100+y;
+// 	traza = "inicio"
+	
+	
+// 	return function(){
+// 		traza += "-g0";
+// 		x++;
+// 		console.log("g0: incremento de x:  "+x);
+
+// 		return console.log("\ttraza: ", traza);;
+// 	}
+// }
+// let f1 = f(-100)
+// f1();
+// f1();
